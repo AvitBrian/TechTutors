3b26c59666b4b100ac331d78f111f23e6245e516
AvitBrian <b.mugisha2@alustudent.com>
2023-08-09 02:47:26 +0200

    updates

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ print("this is where the base directory is: ", basedir)
 # Database
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
 #     os.path.join(basedir, "db.sqlite")
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}@{app.config['DB_HOST']}/{app.config['DB_NAME']}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}@{app.config['DB_HOST']}:{app.config['DB_PORT']}/{app.config['DB_NAME']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
