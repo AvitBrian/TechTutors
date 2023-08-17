0b1479f163075544666e8057436b31cddbdebfda
Ryan Apreala <r.apreala@alustudent.com>
2023-08-17 12:42:19 +0000

    correct database logic and code efficiency

--- ussdApp/app.py
+++ ussdApp/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, request
 import mysql.connector
 
@@ -13,110 +14,75 @@ db = mysql.connector.connect(
     password=app.config['DB_PASSWORD'],
     database=app.config['DB_NAME']
 )
+
 if db.is_connected():
-    print("database connection established successfully")
+    print("Database connection established successfully")
 
-print(db.name)
-
+print(db.database)
 
 @app.route('/ussd', methods=['POST'])
 def ussd_handler():
-    """
-    Handles incoming USSD requests and generates appropriate responses.
-
-    Returns:
-        str: USSD response text
-    """
     session_id = request.form['sessionId']
     user_input = request.form['text']
 
-    if user_input == '*1188#':
-        # Initial menu for authentication
-        response = 'CON Welcome to TechTutors USSD!\n'
-        response += '1. Culture\n'
-        response += '2. Entertainment\n'
-        response += '3. Fashion\n'
-        response += '4. Sports\n'
-        response += '5. Technology\n'
-        response += '6. Add a Category for the Future\n'
+    if user_input == '*1188*':
+        # List categories
+        response = 'CON Select a category:\n'
+        categories = get_categories()
+        for idx, category in enumerate(categories, start=1):
+            response += f'{idx}. {category}\n'
+        response += f'{len(categories) + 1}. Request a new category\n'
     else:
-        # Handle user input using recursive function
+        # Handle user input
         response = handle_user_input(user_input)
 
     return response
 
-
-def handle_user_input(user_input):
-    """
-    Handles user input and generates USSD responses.
-
-    Args:
-        user_input (str): User's input
-
-    Returns:
-        str: USSD response text
-    """
-    if user_input == '1':
-        return get_random_content('Culture')
-    elif user_input == '2':
-        return get_random_content('Entertainment')
-    elif user_input == '3':
-        return get_random_content('Fashion')
-    elif user_input == '4':
-        return get_random_content('Sports')
-    elif user_input == '5':
-        return get_random_content('Technology')
-    elif user_input == '6':
-        return 'CON Enter the name of the category you would like to see in the future:\n'
-    elif user_input.startswith('6*'):
-        new_category = user_input[2:]
-        return save_new_category_recursive(new_category)
-    else:
-        return 'END Invalid input. Please try again.'
-
+def get_categories():
+    cursor = db.cursor()
+    query = "SELECT name FROM category"
+    cursor.execute(query)
+    categories = [row[0] for row in cursor.fetchall()]
+    cursor.close()
+    return categories
 
 def get_random_content(category):
-    """
-    Retrieves random content from the specified category.
-
-    Args:
-        category (str): Category name
-
-    Returns:
-        str: USSD response text
-    """
-    cursor = db.cursor(dictionary=True)
-    query = f"SELECT description FROM content WHERE category = '{category}' ORDER BY RAND() LIMIT 1"
+    cursor = db.cursor()
+    query = f"SELECT content FROM bitcontent WHERE category_name = '{category}' ORDER BY RAND() LIMIT 1"
     cursor.execute(query)
     result = cursor.fetchone()
     cursor.close()
 
     if result:
-        return f'CON {category}:\n{result["description"]}\n'
+        return f'CON {category}:\n{result[0]}\n'
     else:
         return 'END No content available for this category.\n'
 
-
-def save_new_category_recursive(category):
-    """
-    Adds a new category to the database.
-
-    Args:
-        category (str): Category name
-
-    Returns:
-        str: USSD response text
-    """
+def save_new_category_request(category):
     cursor = db.cursor()
-    query = f"INSERT INTO categories (name) VALUES ('{category}')"
+    query = f"INSERT INTO requests (category_name) VALUES ('{category}')"
     try:
         cursor.execute(query)
         db.commit()
         cursor.close()
-        return 'END Category added successfully!\n'
+        return 'END Category request added successfully!\n'
     except Exception as e:
-        return f'END Error adding category: {str(e)}\n'
+        return f'END Error adding category request: {str(e)}\n'
 
+def handle_user_input(user_input):
+    if user_input.isdigit():
+        categories = get_categories()
+        selection = int(user_input)
+        if 1 <= selection <= len(categories):
+            selected_category = categories[selection - 1]
+            return get_random_content(selected_category)
+        elif selection == len(categories) + 1:
+            return 'CON Enter the name of the category you would like to request:\n'
+    elif user_input.startswith('6*'):
+        new_category = user_input[2:]
+        return save_new_category_request(new_category)
+    
+    return 'END Invalid input. Please try again.\n'
 
 if __name__ == '__main__':
     app.run(debug=True)
